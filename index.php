fdfbf349c7051709a2855a9f485cca6a44cc9cf5
Alexandr777 <AlexandrTatkov2002@yandex.ru>
2022-05-11 12:18:08 +0300

    Second

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = 'Alexandr'; // укажите здесь ваше имя
+$user_name = 'Alexandr'; $vid = array("Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты","Разное");
+$Info=[
+    ['Name'=>'2014 Rossignol District Snowboard', 'Cat'=>"$vid[0]", 'Price'=>'10999', 'URL'=>'img/lot-1.jpg'],
+    ['Name'=>'DC Ply Mens 2016/2017 Snowboard', 'Cat'=>"$vid[0]", 'Price'=>'159999', 'URL'=>'img/lot-2.jpg'],
+    ['Name'=>'2014 Rossignol District Snowboard', 'Cat'=>"$vid[1]", 'Price'=>'8000', 'URL'=>'img/lot-3.jpg'],
+    ['Name'=>'2014 Rossignol District Snowboard', 'Cat'=>"$vid[2]", 'Price'=>'10999', 'URL'=>'img/lot-4.jpg'],
+    ['Name'=>'2014 Rossignol District Snowboard', 'Cat'=>"$vid[4]", 'Price'=>'7500', 'URL'=>'img/lot-5.jpg'],
+    ['Name'=>'2014 Rossignol District Snowboard', 'Cat'=>"$vid[5]", 'Price'=>'7500', 'URL'=>'img/lot-6.jpg']
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
